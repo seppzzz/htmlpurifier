4b6e2568cc9cdca924e93870841c90a4755b0abc
seppzzz <till.moser@ufg.ac.at>
2023-09-20 22:42:02 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+# HtmlPurifier
+
+A simple wrapper around HtmlPurifier [http://htmlpurifier.org/]
+
+## Introduction
+
+This module adds a wrapper named Purifier around the famous HtmlPurifier. You can use HtmlPurifier by itself, or use some 
+of the custom function to have clean HTML or XHTML code, TXT code or to strip the [embed] tags. It's also possible to extend
+the Purifier class to add your own purifier functions.
+
+## Requirements
+
+ * SilverStripe >= 4.1 < 5.0
+
+ For 3.x installations refer to the 1.x version
+
+## Installation
+
+Install the module through [composer](http://getcomposer.org):
+
+	composer require seppzzz/htmlpurifier
+	composer update
+
+## Usage
+
+Whenever you need to purify your HTML code, eg. during OnBeforeWrite, OnAfterWrite or in a custom search, just call the wrapper function:
+
+```php
+$content = Purifier::PurifyTXT($p->Content);
+$content = Purifier::RemoveEmbed($content);
+```
---
+++ src/Purifier.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace seppzzz\HtmlPurifier;
+use SilverStripe\Core\Config\Config;
+
+/**
+ * Standard HTML purifier
+ */
+class Purifier {
+
+	/**
+	 * Set custom options for HTML Purifier
+	 * @link http://htmlpurifier.org/live/configdoc/plain.html
+	 * @config
+	 * @var array
+	 */
+	private static $html_purifier_config = [];
+
+	/**
+	 * Get the HTML Purifier config
+	 * @return \HTMLPurifier_Config
+	 */
+	private static function GetHTMLPurifierConfig() {
+		$defaultConfig = \HTMLPurifier_Config::createDefault();
+
+		$customConfig = Config::inst()->get('seppzzz\HtmlPurifier\Purifier', 'html_purifier_config');
+
+		if(is_array($customConfig)) {
+			foreach ($customConfig as $key => $value) {
+				$defaultConfig->set($key, $value);
+			}
+		}
+
+		return $defaultConfig;
+	}
+
+	/**
+	 * Standard HTML purifier
+	 * @param String $html HTML to purify
+	 * @param String $encoding
+	 * @return String Valid HTML string
+	 */
+	public static function Purify($html = null, $encoding = 'UTF-8') {
+
+		$config = self::GetHTMLPurifierConfig();
+		$config->set('Core.Encoding', $encoding);
+		$config->set('HTML.Allowed', 'span,p,br,a,h1,h2,h3,h4,h5,strong,em,u,ul,li,ol,hr,blockquote,sub,sup,p[class],img');
+		$config->set('HTML.AllowedElements', array('span', 'p', 'br', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'strong', 'em', 'u', 'ul', 'li', 'ol', 'hr', 'blockquote', 'sub', 'sup', 'img'));
+		$config->set('HTML.AllowedAttributes', 'style,target,title,href,class,src,border,alt,width,height,title,name,id');
+		$config->set('CSS.AllowedProperties', 'text-align,font-weight,text-decoration');
+		$config->set('AutoFormat.RemoveEmpty', true);
+		$config->set('Attr.ForbiddenClasses', array('MsoNormal'));
+
+		$purifier = new \HTMLPurifier($config);
+		return $cleanCode = $purifier->purify($html);
+	}
+
+	/**
+	 * XHTML 1.0 Strict purifier
+	 * @param String $html HTML to purify
+	 * @param String $encoding
+	 * @return String XTML 1.0 Strict validating string
+	 */
+	public static function PurifyXHTML($html = null, $encoding = 'UTF-8') {
+
+		$config = self::GetHTMLPurifierConfig();
+		$config->set('Core.Encoding', $encoding);
+		$config->set('HTML.Doctype', 'XHTML 1.0 Strict');
+		$config->set('HTML.TidyLevel', 'heavy'); // all changes, minus...
+		$config->set('CSS.AllowedProperties', array());
+		$config->set('Attr.AllowedClasses', array());
+		$config->set('HTML.Allowed', null);
+		$config->set('AutoFormat.RemoveEmpty', true); // Remove empty tags
+		$config->set('AutoFormat.Linkify', true);	// add <A> to links
+		$config->set('AutoFormat.AutoParagraph', true);
+		$config->set('HTML.ForbiddenElements', array('span', 'center'));
+		$config->set('Core.EscapeNonASCIICharacters', true);
+		$config->set('Output.TidyFormat', true);
+
+		$purifier = new \HTMLPurifier($config);
+		$html = $purifier->purify($html);
+
+		// Rimpiazzo le parentesi quadre
+		$html = str_ireplace("%5B", "[", $html);
+		$html = str_ireplace("%5D", "]", $html);
+
+		return $html;
+	}
+	
+	/**
+	 * Strip all HTML tags, like strip_tags, but encoding safe
+	 * @param String $html HTML to purify
+	 * @param String $encoding
+	 * @return String The input string without any html tags
+	 */
+	public static function PurifyTXT($html = null, $encoding = 'UTF-8') {
+		
+		$config = self::GetHTMLPurifierConfig();
+		$config->set('Core.Encoding', $encoding);
+		$config->set('HTML.Allowed', null); // Allow Nothing
+		$config->set('HTML.AllowedElements', array());
+		$purifier = new \HTMLPurifier($config);
+		return $purifier->purify($html);
+	}
+
+	/**
+	 * Removes Silverstripe [embed] tags
+	 * @param String $html
+	 * @param String $encoding
+	 * @return String
+	 */
+	public static function RemoveEmbed($html = null, $encoding = 'UTF-8') {
+		
+		$pattern = '/\[embed.*\].*\[\/embed\]/';
+		return preg_replace($pattern, '', $html);
+	}
+	
+}
